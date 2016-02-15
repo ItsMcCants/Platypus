06c6fc718b1a78efea62d1a89494c5a5f696d78e
John McCants <johnmccants@johns-macbook-pro.local>
2016-02-15 10:47:41 -0800

    first

---
+++ Platypus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Platypus
+//
+//  Created by John McCants on 2/15/16.
+//  Copyright © 2016 John McCants. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var Joe: NSString = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
