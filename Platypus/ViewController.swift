39239b022c1cdc5921c6853072556d7c5f102961
j-n4m4573 <jmg220p@gmail.com>
2016-02-22 11:32:24 -0800

    new branch test commit

--- Platypus/ViewController.swift
+++ Platypus/ViewController.swift
@@ -30,11 +30,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+   
 }
 
